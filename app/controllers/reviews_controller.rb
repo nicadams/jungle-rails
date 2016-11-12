db8bc5b5b09a4551d123a731216d47a45cebf198
nicadams <nic.adams@hotmail.com>
2016-11-12 00:14:28 +0000

    Review form logging to DB and only works for logged in users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,23 @@
 class ReviewsController < ApplicationController
 
+  def create
+    @product = Product.find(params[:product_id])
+    @review = Review.new(review_params)
+    @review.product_id = @product.id
+    # @review.user = current_user
+    if @review.save
+      # redirect_to "/products/#{params[:product_id]}"
+      redirect_to product_path(@product)
+    else
+      render :'products/show'
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def review_params
+      params.require(:review).permit(:rating, :description, :user_id)
+    end
 end
+
