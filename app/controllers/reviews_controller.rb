08f0bb862b1e745fd3da9e1173576917466098c1
nicadams <nic.adams@hotmail.com>
2016-11-12 02:28:48 +0000

    Can delete reviews if current user is the review user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
 
   def create
     @product = Product.find(params[:product_id])
     @review = Review.new(review_params)
     @review.product_id = @product.id
-    # @review.user = current_user
+    @review.user = current_user
     if @review.save
       # redirect_to "/products/#{params[:product_id]}"
       redirect_to product_path(@product)
@@ -12,6 +13,15 @@ class ReviewsController < ApplicationController
       render :'products/show'
     end
   end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to product_path(@product), notice: 'Review deleted!'
+
+  end
+
 
   private
 
